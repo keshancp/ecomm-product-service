a12586ab93f230ddaffc7d769d054f750abe8a3c
keshancp <keshanc.perera@gmail.com>
2022-10-04 21:08:28 +0530

    initial commit for ecomm-service

---
+++ src/main/java/com/ecomm/ecommservice/EcommServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.ecommservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomm/ecommservice/EcommServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.ecommservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
